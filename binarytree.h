add472ddeb40df945333f50c0090c93f5695ee23
Nami Arj <namiarjmandi@gmail.com>
2024-01-01 09:40:51 +0000

    Update binarytree.h

--- binarytree.h
+++ binarytree.h
@@ -13,17 +13,16 @@ struct node {
     struct addr addr;
     uint32_t count;
     uint64_t bytes;
-    uint64_t protos;
     char src_s[16];
     char dst_s[16];
     struct node *left;
     struct node *right;
 };
 
-struct node* binarytree_find(struct node*, const uint32_t, const uint32_t);
-void binarytree_free(struct node*);
-struct node* binarytree_init(const uint32_t, const uint32_t);
-struct node* binarytree_insert(struct node*, const uint32_t, const uint32_t);
-void binarytree_print(FILE*, struct node*);
+struct node *bt_find(struct node*, const uint32_t, const uint32_t);
+void bt_free(struct node*);
+struct node *bt_init(const uint32_t, const uint32_t);
+struct node *bt_insert(struct node*, const uint32_t, const uint32_t);
+void bt_print(FILE*, struct node*);
 
 #endif
